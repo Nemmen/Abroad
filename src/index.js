63b02d7351e88e6d57040f5b2848de0cc1cd8e2a
Nemmen <aryankammboz@gmail.com>
2024-10-23 11:37:19 +0530

    font changed

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import { store } from './views/mainpages/redux/Store';
 import App from './App';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '@fontsource/roboto/400.css'; // Regular font weight
+import '@fontsource/roboto/500.css'; // Medium font weight
+import '@fontsource/roboto/700.css'; // Bold font weight
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
