ac43fb8d385846d986a536c879861ca19189891c
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-10-23 10:13:31 +0530

    tailwind done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './assets/css/App.css';
+// import './assets/css/App.css';
+import './index.css'
 import { Provider } from 'react-redux';
 import { store } from './views/mainpages/redux/Store';
 import App from './App';
