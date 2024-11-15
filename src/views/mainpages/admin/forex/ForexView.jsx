1d32159aa62fad6a603e35700cc9da41dc6b5762
Nemmen <aryankammboz@gmail.com>
2024-11-15 13:14:16 +0530

    forex/gic - admin done

--- src/views/mainpages/admin/forex/ForexView.jsx
+++ src/views/mainpages/admin/forex/ForexView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Box,
   Flex,
@@ -9,7 +9,18 @@ import {
   Icon,
   useColorModeValue,
 } from '@chakra-ui/react';
-import { FiFileText, FiUser, FiGlobe, FiDollarSign, FiFile, FiCheckSquare, FiPercent, FiFolder } from 'react-icons/fi';
+import {
+  FiFileText,
+  FiUser,
+  FiGlobe,
+  FiDollarSign,
+  FiFile,
+  FiCheckSquare,
+  FiPercent,
+  FiFolder,
+} from 'react-icons/fi';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 // Sample data for display, including documents
 const sampleData = {
@@ -62,7 +73,32 @@ const fieldIcons = {
 };
 
 function ForexView() {
-  const formData = sampleData;
+  const { id } = useParams();
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/auth/viewAllForexForms`)
+      .then((response) => {
+        console.log('Response received:', response.data);
+        if (response.data.success) {
+          const formData1 = response.data.forexForms.find(
+            (form) => form._id === id,
+          );
+          if (formData1) {
+            setFormData(formData1);
+            
+          } else {
+            console.error('Form data not found for ID:', id);
+          }
+        } else {
+          console.error('Request was not successful:', response.data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, [id]);
 
   // Define color mode values
   const labelColor = useColorModeValue('gray.500', 'gray.400');
@@ -88,80 +124,108 @@ function ForexView() {
         </Heading>
       </Flex>
 
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-        {Object.entries(formData).map(([label, value], index) => (
-          // Render general fields, excluding 'documents'
-          label !== 'documents'  && (
-            <VStack key={index} align="start" spacing={2} w="full"   gridColumn={label === 'offerLetterFile' ? 'span 2' : 'span 1'}>
-              <Flex align="center">
-                <Icon as={fieldIcons[label] || FiFileText} color="blue.500" mr={2} />
-                <Text fontSize="sm" fontWeight="medium" color={labelColor}>
-                  {label.replace(/([A-Z])/g, ' $1')}
-                </Text>
-              </Flex>
+      {Object.keys(formData).length > 0 && (
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          {Object.entries(formData).map(
+            ([label, value], index) =>
+              // Render general fields, excluding 'documents'
+            label !== '__v' && label !=='_id' && label !== 'documents' && (
+                <VStack
+                  key={index}
+                  align="start"
+                  spacing={2}
+                  w="full"
+                >
+                  <Flex align="center">
+                    <Icon
+                      as={fieldIcons[label] || FiFileText}
+                      color="blue.500"
+                      mr={2}
+                    />
+                    <Text fontSize="sm" fontWeight="medium" color={labelColor}>
+                      {label.replace(/([A-Z])/g, ' $1')}
+                    </Text>
+                  </Flex>
+                  <Box p={4} bg={fieldBgColor} borderRadius="md" width="full">
+                    <Text fontSize="lg" fontWeight="bold" color={valueColor}>
+                    {label === 'date'
+                      ? new Date(value).toLocaleDateString('en-GB') // Format as DD/MM/YYYY
+                      : value}
+                    </Text>
+                  </Box>
+                </VStack>
+              ),
+          )}
+
+          {/* Document section */}
+          <Box gridColumn="span 2" mt={6}>
+            <Flex align="center" mb={4}>
+              <Icon as={FiFolder} color="blue.500" mr={2} />
+              <Heading as="h4" fontSize="2xl" color="blue.600">
+                Documents
+              </Heading>
+            </Flex>
+            {formData.documents.map((doc, index) => (
               <Box
+                key={index}
+                mb={4}
                 p={4}
                 bg={fieldBgColor}
                 borderRadius="md"
-                width="full"
+                boxShadow="md"
               >
-                <Text fontSize="lg" fontWeight="bold" color={valueColor}>
-                  {value}
-                </Text>
+                <SimpleGrid columns={{ base: 1, md: 3 }} spacing={4}>
+                  <VStack align="start">
+                    <Flex align="center">
+                      <Icon as={FiUser} color="blue.500" mr={2} />
+                      <Text
+                        fontSize="sm"
+                        fontWeight="medium"
+                        color={labelColor}
+                      >
+                        Document Of
+                      </Text>
+                    </Flex>
+                    <Text fontSize="md" fontWeight="bold" color={valueColor}>
+                      {doc.documentOf}
+                    </Text>
+                  </VStack>
+                  <VStack align="start">
+                    <Flex align="center">
+                      <Icon as={FiFileText} color="blue.500" mr={2} />
+                      <Text
+                        fontSize="sm"
+                        fontWeight="medium"
+                        color={labelColor}
+                      >
+                        Document Type
+                      </Text>
+                    </Flex>
+                    <Text fontSize="md" fontWeight="bold" color={valueColor}>
+                      {doc.documentType}
+                    </Text>
+                  </VStack>
+                  <VStack align="start">
+                    <Flex align="center">
+                      <Icon as={FiFile} color="blue.500" mr={2} />
+                      <Text
+                        fontSize="sm"
+                        fontWeight="medium"
+                        color={labelColor}
+                      >
+                        File Name
+                      </Text>
+                    </Flex>
+                    <Text fontSize="md" fontWeight="bold" color={valueColor}>
+                      {doc.documentFile}
+                    </Text>
+                  </VStack>
+                </SimpleGrid>
               </Box>
-            </VStack>
-          )
-        ))}
-
-        {/* Document section */}
-        <Box gridColumn="span 2" mt={6}>
-          <Flex align="center" mb={4}>
-            <Icon as={FiFolder} color="blue.500" mr={2} />
-            <Heading as="h4" fontSize="2xl" color="blue.600">
-              Documents
-            </Heading>
-          </Flex>
-          {formData.documents.map((doc, index) => (
-            <Box key={index} mb={4} p={4} bg={fieldBgColor} borderRadius="md" boxShadow="md">
-              <SimpleGrid columns={{ base: 1, md: 3 }} spacing={4}>
-                <VStack align="start">
-                  <Flex align="center">
-                    <Icon as={FiUser} color="blue.500" mr={2} />
-                    <Text fontSize="sm" fontWeight="medium" color={labelColor}>
-                      Document Of
-                    </Text>
-                  </Flex>
-                  <Text fontSize="md" fontWeight="bold" color={valueColor}>
-                    {doc.documentOf}
-                  </Text>
-                </VStack>
-                <VStack align="start">
-                  <Flex align="center">
-                    <Icon as={FiFileText} color="blue.500" mr={2} />
-                    <Text fontSize="sm" fontWeight="medium" color={labelColor}>
-                      Document Type
-                    </Text>
-                  </Flex>
-                  <Text fontSize="md" fontWeight="bold" color={valueColor}>
-                    {doc.documentType}
-                  </Text>
-                </VStack>
-                <VStack align="start">
-                  <Flex align="center">
-                    <Icon as={FiFile} color="blue.500" mr={2} />
-                    <Text fontSize="sm" fontWeight="medium" color={labelColor}>
-                      File Name
-                    </Text>
-                  </Flex>
-                  <Text fontSize="md" fontWeight="bold" color={valueColor}>
-                    {doc.documentFile}
-                  </Text>
-                </VStack>
-              </SimpleGrid>
-            </Box>
-          ))}
-        </Box>
-      </SimpleGrid>
+            ))}
+          </Box>
+        </SimpleGrid>
+      )}
     </Box>
   );
 }
