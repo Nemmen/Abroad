4272f39db8d21d90afd0577250ef691a4cb2b38c
Tiwari Lokesh <lokeshtiwari212@gmail.com>
2024-12-17 16:13:25 +0530

    enhance UI of dashboard

--- src/views/admin/marketplace/components/Banner.js
+++ src/views/admin/marketplace/components/Banner.js
@@ -2,24 +2,27 @@ import React from "react";
 
 // Chakra imports
 import { Button, Flex, Link, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom"; // Import the useNavigate hook
 
 // Assets
 import banner from "assets/img/nfts/NftBanner1.png";
 
 export default function Banner() {
-  // Chakra Color Mode
+  const navigate = useNavigate(); // Initialize the navigate function
+
   return (
     <Flex
-      direction='column'
+      direction="column"
       bgImage={banner}
-      bgSize='cover'
+      bgSize="cover"
       py={{ base: "30px", md: "56px" }}
       px={{ base: "30px", md: "64px" }}
-      borderRadius='30px'>
+      borderRadius="30px"
+    >
       <Text
         fontSize={{ base: "24px", md: "34px" }}
-        color='white'
-        mb='14px'
+        color="white"
+        mb="14px"
         maxW={{
           base: "100%",
           md: "64%",
@@ -28,13 +31,14 @@ export default function Banner() {
           "2xl": "50%",
           "3xl": "42%",
         }}
-        fontWeight='700'
-        lineHeight={{ base: "32px", md: "42px" }}>
-        Mangage your user through this dashboard
+        fontWeight="700"
+        lineHeight={{ base: "32px", md: "42px" }}
+      >
+        Manage your user through this dashboard
       </Text>
       <Text
-        fontSize='md'
-        color='#E3DAFF'
+        fontSize="md"
+        color="#E3DAFF"
         maxW={{
           base: "100%",
           md: "64%",
@@ -43,28 +47,22 @@ export default function Banner() {
           "2xl": "46%",
           "3xl": "34%",
         }}
-        fontWeight='500'
-        mb='40px'
-        lineHeight='28px'>
-        Mangage you all Services at on place
+        fontWeight="500"
+        mb="40px"
+        lineHeight="28px"
+      >
+        Manage all your services in one place
       </Text>
-      <Flex align='center'>
-        <Button
-          bg='white'
-          color='black'
-          _hover={{ bg: "whiteAlpha.900" }}
-          _active={{ bg: "white" }}
-          _focus={{ bg: "white" }}
-          fontWeight='500'
-          fontSize='14px'
-          py='20px'
-          px='27'
-          me='38px'>
-         Go to agent
-        </Button>
-        <Link>
-          <Text color='white' fontSize='sm' fontWeight='500'>
-           Go to Profile
+      <Flex align="center">
+      <Link onClick={() => navigate("/admin/gic")}>
+          <Text className="bg-white pe-7 ps-7 pt-2 pb-2  rounded-3xl text-gray-700 font-semibold text-[15px]">
+            Go to GIC
+          </Text>
+        </Link>
+        {/* Link to the GIC page */}
+        <Link onClick={() => navigate("/admin/forex")}>
+          <Text className="ms-2 bg-white pe-5 ps-5 pt-2 pb-2  rounded-3xl text-gray-700 font-semibold text-[15px]">
+            Go to FOREX
           </Text>
         </Link>
       </Flex>
