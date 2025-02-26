7f9e2e630d3ac9f1871b0026d449e22927dbb02f
Nemmen <aryankammboz@gmail.com>
2025-02-26 16:09:47 +0530

    sc

--- src/views/mainpages/login_signup/Login.jsx
+++ src/views/mainpages/login_signup/Login.jsx
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { post } from '../services/ApiEndpoint';
-import { Button, useToast, Spinner } from '@chakra-ui/react';
+import { Button, useToast } from '@chakra-ui/react'; // Import useToast from Chakra UI
 import { useDispatch, useSelector } from 'react-redux';
 import { SetUser } from '../redux/AuthSlice';
 import loginimg from '../../../assets/img/auth/login.png';
-import { HiEye, HiEyeOff } from 'react-icons/hi';
+import { HiEye, HiEyeOff } from 'react-icons/hi'; // Import eye icons from react-icons
+import { Spinner } from '@chakra-ui/react';
+
 
 export default function Login() {
+  const user = useSelector((state) => state.Auth);
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  const toast = useToast();
+  const [showPassword, setShowPassword] = useState(false); // State for password visibility
   const navigate = useNavigate();
+
+  // Initialize the toast
+  const toast = useToast();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
-      const request = await post(
-        'https://abroad-backend-gray.vercel.app/auth/login',
-        { email, password },
-        { credentials: 'include' },
-      );
+      const request = await post('http://localhost:4000/auth/login', {
+        email,
+        password,
+      });
       const response = request.data;
 
       if (request.status === 200) {
@@ -32,12 +36,14 @@ export default function Login() {
         localStorage.setItem('user_role', response.user.role);
         setLoading(false);
 
+        // Navigate based on user role
         if (response.user.role === 'admin') {
           navigate('/admin');
         } else if (response.user.role === 'user') {
           navigate('/agent');
         }
 
+        // Show success toast
         toast({
           title: 'Login Successful',
           description: 'Welcome back!',
@@ -45,40 +51,63 @@ export default function Login() {
           duration: 1000,
           isClosable: true,
           position: 'bottom-right',
-          containerStyle: { width: '400px' },
+          containerStyle: {
+            width: '400px',
+          },
         });
+        if (response.status === 401) {
+          toast({
+            title: 'Login Failed',
+            description: response.user.message,
+            status: 'error',
+            duration: 1000,
+            isClosable: true,
+            position: 'bottom-right',
+            containerStyle: {
+              width: '400px',
+            },
+          });
+          setLoading(false);
+        }
 
         dispatch(SetUser(response.user));
       }
     } catch (error) {
+      // Show error toast
       setLoading(false);
       toast({
         title: 'Login Failed',
-        description: error.response?.data?.message || 'Something went wrong',
-        status: 'error',
+        description: error.response.data.message,
+        status:
+          error.response.data.message == 'Invalid credentials'
+            ? 'error'
+            : 'warning',
         duration: 1000,
         isClosable: true,
         position: 'bottom-right',
-        containerStyle: { width: '400px' },
+        containerStyle: {
+          width: '400px',
+        },
       });
+      console.log(error);
     }
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="flex bg-white rounded-lg shadow-lg overflow-hidden max-w-4xl h-[655px]">
-        <div className="w-1/2 bg-blue-100 flex justify-center items-center">
+      <div className="flex bg-white rounded-lg md:shadow-lg  overflow-hidden max-w-4xl h-[655px]">
+        <div className="hidden md:flex w-1/2 bg-blue-100 justify-center items-center">
           <img
             src={loginimg}
             alt="Person on laptop"
             className="w-[40vw] h-auto"
           />
         </div>
-        <div className="p-10 flex flex-col justify-center w-[450px]">
+        <div className="p-12 flex flex-col justify-center w-[450px]">
           <h2 className="text-2xl font-semibold text-gray-800">Sign In</h2>
           <p className="text-sm text-gray-600 mb-6">Unlock your world.</p>
           <form onSubmit={handleSubmit}>
-            <div className="mb-3">
+            <div className="mb-4">
               <label
                 htmlFor="email"
                 className="block text-sm font-semibold text-gray-800"
@@ -94,7 +123,7 @@ export default function Login() {
                 className="w-full px-4 py-2 rounded-lg border border-gray-300 mt-1"
               />
             </div>
-            <div className="mb-5 relative">
+            <div className="mb-6 relative">
               <label
                 htmlFor="password"
                 className="block text-sm font-semibold text-gray-800"
@@ -102,7 +131,7 @@ export default function Login() {
                 Password
               </label>
               <input
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? 'text' : 'password'} // Toggle input type based on state
                 id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -110,7 +139,7 @@ export default function Login() {
                 className="w-full px-4 py-2 rounded-lg border border-gray-300 mt-1"
               />
               <span
-                onClick={() => setShowPassword(!showPassword)}
+                onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
                 className="absolute right-3 top-9 cursor-pointer"
               >
                 {showPassword ? (
@@ -120,13 +149,10 @@ export default function Login() {
                 )}
               </span>
             </div>
-            <p className="text-end text-blue-600 hover:underline mb-3">
-              <Link to="/auth/forgot-password">Forgot Password?</Link>
-            </p>
             {loading ? (
-              <div className="text-center">
-                <Spinner size="lg" color="blue.500" />
-              </div>
+             <div className='text-center'>
+               <Spinner size="lg" color="blue.500" />
+             </div>
             ) : (
               <button
                 type="submit"
@@ -137,11 +163,12 @@ export default function Login() {
             )}
           </form>
           <p className="text-center text-slate-500 text-[16px] mx-auto flex pt-3">
-            Don't have an account?{' '}
+            Don't have an account ?{' '}
             <Link
               to="/auth/signup"
-              className="block text-center text-blue-600 hover:underline ml-2"
+              className="block text-center text-blue-600 hover:underline ml-2 "
             >
+              {' '}
               Sign up
             </Link>
           </p>
